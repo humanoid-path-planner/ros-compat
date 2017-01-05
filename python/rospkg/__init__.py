ac793f2745fafefe0cbe7f46bd97547da5d9eb08
Florent Lamiraux <florent@laas.fr>
2017-01-05 19:02:14 +0100

    Install missing directory.

---
+++ python/rospkg/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2017 CNRS
+# Author: Florent Lamiraux
+#
+# This file is part of ros-compat.
+# ros-compat is free software: you can redistribute it
+# and/or modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation, either version
+# 3 of the License, or (at your option) any later version.
+#
+# ros-compat is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Lesser Public License for more details.  You should have
+# received a copy of the GNU Lesser General Public License along with
+# ros-compat.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+from rospack import RosPack
